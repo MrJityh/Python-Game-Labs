e16bfa2a9884d68e7942e923e527db5b5876c845
James Pierce <jmp7247@psu.edu>
2024-03-14 09:00:03 -0400

    lab10 finished

--- src/lab10/lab10.py
+++ src/lab10/lab10.py
@@ -1,16 +1,17 @@
 """ Lab 10: Save people
 You can save people from heart disease by training a model to predict whether a person has heart disease or not.
-The dataset is available at src/lab8/heart.csv
+The dataset is available at src/lab10/heart.csv
 Train a model to predict whether a person has heart disease or not and test its performance.
 You can usually improve the model by normalizing the input data. Try that and see if it improves the performance. 
 """
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler
 import pandas as pd
 import numpy as np
 
-data = pd.read_csv("src/lab8/heart.csv")
+data = pd.read_csv("src/lab10/heart.csv")
 
 # Transform the categorical variables into dummy variables.
 print(data.head())
@@ -25,13 +26,21 @@ x_train, x_test, y_train, y_test = train_test_split(
 )
 
 """ Train a sklearn model here. """
+#Defines and trains model
+sklearn_model = KNeighborsClassifier()
+sklearn_model.fit(x_train, y_train)
 
-sklearn_model = None
-
-# Accuracy
+# Accuracy of model with raw data
 print("Accuracy of model: {}\n".format(sklearn_model.score(x_test, y_test)))
 
-
 """ Improve the model by normalizing the input data. """
+# Normalizes the values, using a scaler as opposed to X' = (X-Xmin)/(Xmax-Xmin)
+scaler = StandardScaler()
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test)
 
+sklearn_model = KNeighborsClassifier()
+sklearn_model.fit(x_train, y_train)
+
+#Accuracy of model with normalized data
 print("Accuracy of improved model: {}\n".format(sklearn_model.score(x_test, y_test)))
